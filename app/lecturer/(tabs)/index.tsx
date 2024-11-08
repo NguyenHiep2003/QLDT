77766edc9c51116c57841c8c298ae74f1708f798
Nguyen Huu Duc <diemannschaft2003@gmail.com>
2024-11-09 00:22:03 +0700

    Edit class API completed

--- app/lecturer/(tabs)/index.tsx
+++ app/lecturer/(tabs)/index.tsx
@@ -1,12 +1,10 @@
-import { router } from 'expo-router';
+import {router, useFocusEffect} from 'expo-router';
 import {
     SafeAreaView,
-    View,
     FlatList,
     StyleSheet,
     Text,
     StatusBar,
-    TouchableOpacity,
 } from 'react-native';
 import { ClassInfo } from '@/types/generalClassInfor';
 import { ClassCard } from '@/components/ClassCard';
@@ -19,8 +17,7 @@ export default function Index() {
     const [classes, setClasses] = useState<ClassInfo[]>([]);
     const [dialogUnauthorizedVisible, setDialogUnauthorizedVisible] = useState(false);
 
-
-    useEffect(() => {
+    useFocusEffect(() => {
         getClassList(ROLES.STUDENT)
             .then((classes) => setClasses(classes))
             .catch((err) => {
@@ -28,7 +25,8 @@ export default function Index() {
                     setDialogUnauthorizedVisible(true);
                 }
             });
-    }, []);
+    });
+
     return (
         <SafeAreaView style={styles.container}>
             <Text style={styles.title}>Danh sách lớp giảng dạy</Text>
@@ -38,7 +36,10 @@ export default function Index() {
                     <ClassCard
                         data={item}
                         onPress={() =>
-                            router.push(`/lecturer/classes/${item.class_id}`)
+                            router.push({
+                                pathname: `/lecturer/classes/${item.class_id}`,
+                                params: { class_id: item.class_id },
+                            })
                         }
                     />
                 )}
