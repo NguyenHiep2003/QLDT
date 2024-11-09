db86f917a25b37c86b0817849c9da261642d6f5d
Quang Vu <lyvu2804@gmail.com>
2024-11-09 22:31:53 +0700

    update register/management-class

--- services/api-calls/classes.ts
+++ services/api-calls/classes.ts
@@ -11,3 +11,24 @@ export async function getClassList(role: ROLES) {
     });
     return response.data;
 }
+
+export async function getClassInfo(class_id: string) {
+    const profile = await getProfileLocal();
+    if (!profile) throw new Error('Profile not found');
+
+    const body = {
+        role: ROLES.STUDENT,
+        account_id: profile.id,
+        class_id: class_id,
+    };
+
+    const response = await instance.post('/it5023e/get_basic_class_info', body);
+    return response.data;
+}
+
+export async function registerClass(class_ids: string[]) {
+    const response = await instance.post('/it5023e/register_class', {
+        class_ids: class_ids,
+    });
+    return response.data;
+}
