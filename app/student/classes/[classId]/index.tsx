61400295024ce825f4bed2832b354bbaf5ade376
Nguyen Hiep <mccter2003@gmail.com>
2024-11-10 21:18:48 +0700

    update detail class info

--- app/student/classes/[classId]/index.tsx
+++ app/student/classes/[classId]/index.tsx
@@ -1,40 +1,95 @@
+import { ServiceCard } from '@/components/ServiceCard';
+import { getClassInfo } from '@/services/api-calls/classes';
 import { ClassInfo } from '@/types/generalClassInfor';
-import { Link, Redirect, useLocalSearchParams } from 'expo-router';
-import { useState } from 'react';
-import {
-    Button,
-    StatusBar,
-    Text,
-    StyleSheet,
-    ScrollView,
-    View,
-} from 'react-native';
+import { Link, useLocalSearchParams } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { Text, StyleSheet, ScrollView, View } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-export default function Index() {
+export default function StudentClassDetail() {
+    const servicesCardConfig = [
+        { name: 'description', title: 'Tài liệu' },
+        { name: 'assignment', title: 'Bài tập' },
+        { name: 'sick', title: 'Nghỉ học' },
+    ];
+    const classStatusMap = {
+        ACTIVE: 'Đang diễn ra',
+        COMPLETED: 'Đã hoàn thành',
+        UPCOMING: 'Mở đăng ký',
+    };
     const { classId } = useLocalSearchParams();
     const [classInfo, setClassInfo] = useState<ClassInfo | undefined>(
         undefined
     );
+    const [isShowStudentList, setIsShowStudentList] = useState(false);
+    useEffect(() => {
+        getClassInfo({ class_id: classId as string })
+            .then((response) => setClassInfo(response.data))
+            .catch((err) => console.log(err));
+    }, []);
     return (
         <SafeAreaProvider>
             <SafeAreaView style={styles.container} edges={['top']}>
-                <ScrollView style={styles.scrollView}>
+                <ScrollView
+                    style={styles.scrollView}
+                    nestedScrollEnabled={true}
+                >
                     <View style={styles.classInfo}>
-                        <Text style={styles.title}>Thông tin lớp học</Text>
-                        <Text style={styles.text}></Text>
+                        <Text style={styles.title}>
+                            {classInfo?.class_name}
+                        </Text>
+                        <Text style={styles.text}>
+                            Mã lớp: {classInfo?.class_id}
+                        </Text>
+                        <Text style={styles.text}>
+                            Loại hình: {classInfo?.class_type}
+                        </Text>
+                        <Text style={styles.text}>
+                            Giảng viên: {classInfo?.lecturer_name}
+                        </Text>
+                        <Text style={styles.text}>
+                            Số sinh viên:{' '}
+                            <Text
+                                onPress={() => setIsShowStudentList(true)}
+                                style={{
+                                    textDecorationLine: 'underline',
+                                    fontStyle: 'italic',
+                                    color: 'blue',
+                                }}
+                            >
+                                {classInfo?.student_count}
+                            </Text>
+                        </Text>
+                        <Text style={styles.text}>
+                            Ngày bắt đầu: {classInfo?.start_date}
+                        </Text>
+                        <Text style={styles.text}>
+                            Ngày kết thúc: {classInfo?.end_date}
+                        </Text>
+                        <Text style={styles.text}>
+                            Trạng thái:{' '}
+                            {classInfo?.status
+                                ? classStatusMap[`${classInfo?.status}`]
+                                : undefined}
+                        </Text>
                     </View>
-                    <Text style={styles.text}>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in
-                        reprehenderit in voluptate velit esse cillum dolore eu
-                        fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                        non proident, sunt in culpa qui officia deserunt mollit
-                        anim id est laborum.
-                    </Text>
+                    <View
+                        style={{
+                            flexDirection: 'row',
+                            flexWrap: 'wrap',
+                            justifyContent: 'space-between', // Chia đều khoảng cách giữa các box
+                            alignItems: 'center',
+                        }}
+                    >
+                        {servicesCardConfig.map((val) => (
+                            <View style={{ flex: 1 }}>
+                                <ServiceCard
+                                    title={val.title}
+                                    name={val.name}
+                                ></ServiceCard>
+                            </View>
+                        ))}
+                    </View>
                     <Text>
                         Display class information and service for student
                     </Text>
@@ -63,7 +118,7 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         padding: 20,
         marginVertical: 8,
-        marginHorizontal: 16,
+        marginHorizontal: 10,
         shadowColor: 'black',
         shadowOpacity: 0.1,
         shadowOffset: {
@@ -74,9 +129,30 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 20,
         fontWeight: 'bold',
+        marginBottom: 10,
     },
     text: {
-        fontSize: 19,
-        padding: 12,
+        fontSize: 16,
+        marginBottom: 7,
+    },
+    gridView: {
+        marginTop: 10,
+        flex: 1,
+    },
+    itemContainer: {
+        justifyContent: 'flex-end',
+        borderRadius: 5,
+        padding: 10,
+        height: 150,
+    },
+    itemName: {
+        fontSize: 16,
+        color: '#fff',
+        fontWeight: '600',
+    },
+    itemCode: {
+        fontWeight: '600',
+        fontSize: 12,
+        color: '#fff',
     },
 });
--- types/generalClassInfor.ts
+++ types/generalClassInfor.ts
@@ -1,9 +1,20 @@
+import { TStudentAccount } from './profile';
+
+export enum ClassStatus {
+    ACTIVE = 'ACTIVE',
+    COMPLETED = 'COMPLETED',
+    UPCOMING = 'UPCOMING',
+}
+
 export type ClassInfo = {
     class_id: string;
     class_name: string;
+    attached_code: string;
+    class_type: string;
     lecturer_name: string;
     student_count: number;
     start_date: string;
     end_date: string;
-    status: string;
+    status: ClassStatus;
+    student_accounts: TStudentAccount[]; // can sua lai sau
 };
