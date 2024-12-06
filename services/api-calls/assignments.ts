1b30d17ce05cfeaa11850b0bb67a2e21ba5be858
truongngv2003 <truongnguyenmailbka@gmail.com>
2024-12-06 22:26:32 +0700

    feat: add view survey list, submission grading, and survey management (view, create, edit, delete)

--- services/api-calls/assignments.ts
+++ services/api-calls/assignments.ts
@@ -67,14 +67,22 @@ export async function fetchAssignments(typeOfAssignments: string, classId?: stri
   }
 }
 
-type Submission = {
+export type Submission = {
   id: string;
   assignment_id: string;
   submission_time: string;
   grade?: string;
   file_url?: string;
   text_response?: string;
-  student_account: any;
+  student_account: StudentAccount;
+};
+
+type StudentAccount = {
+  account_id: string;
+  last_name: string;
+  first_name: string;
+  email: string;
+  student_id: string;
 };
 
 type SubmissionResponse = {
@@ -96,7 +104,7 @@ export async function fetchSubmission(assignment_id: string) {
 
     if (code === "9994") {
       console.log(errorResponse);
-      error.setIsVisible(false)
+      error.setIsVisible(false);
     }
     throw error;
   }
@@ -129,9 +137,188 @@ export async function submitSurvey(formData: FormData) {
     const { code, message } = meta;
     if (code === "9999") {
       err.setTitle("Đã quá hạn nộp bài");
-      err.setContent("Bạn không thể nộp bài nữa")
+      err.setContent("Bạn không thể nộp bài nữa");
     }
     console.error(errorResponse);
+    throw err;
+  }
+}
+
+//api-lecturer
+export type Survey = {
+  id: number;
+  title: string;
+  description: string;
+  lecturer_id: string;
+  deadline: string;
+  file_url?: string;
+  class_id: string;
+  class_name?: string;
+};
+
+type SurveyResponse = {
+  data: Survey[];
+  meta: Meta;
+};
+
+export async function fetchSurveys(classId: string) {
+  try {
+    const requestBody: any = { class_id: classId };
+
+    // Gửi request đến API
+    const response: SurveyResponse = await instance.post("/it5023e/get_all_surveys", requestBody);
+    const surveys = response.data.reverse();
+
+    try {
+      const classInfoResponse = await getClassInfo({ class_id: classId });
+      surveys.map((survey) => {
+        return {
+          ...survey,
+          class_name: classInfoResponse.data.class_name, // Gắn `class_name` vào survey
+        };
+      });
+    } catch (error: any) {
+      surveys.map((survey) => {
+        return {
+          ...survey,
+          class_name: "",
+        };
+      });
+    }
+
+    return surveys;
+  } catch (error: any) {
+    // Xử lý lỗi
+    console.error("Error fetching surveys:", error.message);
+    throw error;
+  }
+}
+
+type SurveyResponsesResponse = {
+  data: Submission[];
+  meta: Meta;
+};
+
+export async function fetchSurveyResponses(surveyId: string, submissionId?: string, score?: string) {
+  try {
+    const requestBody: any = { survey_id: surveyId };
+    if (submissionId && score) {
+      requestBody.grade = {
+        score,
+        submission_id: submissionId,
+      };
+    }
+
+    // Gửi request đến API
+    const response: SurveyResponsesResponse = await instance.post("/it5023e/get_survey_response", requestBody);
+    const surveyresponses = response.data;
+
+    return surveyresponses;
+  } catch (err: any) {
+    const errorResponse: ErrorResponse = err.rawError;
+    const { data, meta } = errorResponse;
+    const { code, message } = meta;
+
+    if (code === "9999") {
+      err.setTitle("Không tìm thấy bài tập");
+      err.setContent("Bài tập này hiện không tồn tại hoặc đã bị xóa.");
+    }
+    throw err;
+  }
+}
+
+type CreateSurveyResponse = {
+  data: string;
+  meta: Meta;
+};
+export async function createSurvey(formData: FormData) {
+  try {
+    const token = await getTokenLocal();
+    if (!token) throw new UnauthorizedException();
+
+    formData.append("token", token as string);
+    const response: CreateSurveyResponse = await instance.post("/it5023e/create_survey", formData, {
+      headers: {
+        "no-need-token": true,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (err: any) {
+    const errorResponse: ErrorResponse = err.rawError;
+    const { data, meta } = errorResponse;
+    const { code, message } = meta;
+
+    if (code === "1004") {
+      err.setTitle("Deadline không hợp lệ");
+      err.setContent("Thời gian kết thúc không được trước thời gian hiện tại.");
+    }
+
+    throw err;
+  }
+}
+
+type EditSurveyResponse = {
+  data: Survey;
+  meta: Meta;
+};
+export async function editSurvey(formData: FormData) {
+  try {
+    const token = await getTokenLocal();
+    if (!token) throw new UnauthorizedException();
+
+    formData.append("token", token as string);
+    const response: EditSurveyResponse = await instance.post("/it5023e/edit_survey", formData, {
+      headers: {
+        "no-need-token": true,
+        "Content-Type": "multipart/form-data",
+      },
+    });
+
+    return response.data;
+  } catch (err: any) {
+    const errorResponse: ErrorResponse = err.rawError;
+    const { data, meta } = errorResponse;
+    const { code, message } = meta;
+    if (code === "1004") {
+      err.setTitle("Deadline không hợp lệ");
+      err.setContent("Thời gian kết thúc không được trước thời gian hiện tại.");
+    }
+
+    if (code === "9999") {
+      err.setTitle("Không thể sửa bài tập");
+      err.setContent("Bài tập này hiện không tồn tại hoặc đã bị xóa.");
+    }
+    throw err;
+  }
+}
+
+type DeleteSurveyResponse = {
+  data: string;
+  meta: Meta;
+};
+export async function deleteSurvey(surveyId: string) {
+  try {
+    const response: DeleteSurveyResponse = await instance.post("/it5023e/delete_survey", {
+      survey_id: surveyId,
+    });
+    const status = response.data;
+
+    return status;
+  } catch (err: any) {
+    const errorResponse: ErrorResponse = err.rawError;
+    const { data, meta } = errorResponse;
+    const { code, message } = meta;
+
+    if (code === "1004") {
+      err.setTitle("Không thể xóa bài tập");
+      err.setContent("Không thể xóa bài tập khi có người Đã nộp bài.");
+    }
+    if (code === "9999") {
+      err.setTitle("Không thể xóa bài tập");
+      err.setContent("Bài tập này hiện không tồn tại hoặc đã bị xóa.");
+    }
     throw err;
   }
 }
