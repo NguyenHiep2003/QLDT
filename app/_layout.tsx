bcd42b89ec6f8073e30df7019bbb4d6d78ca7aa8
Nguyen Huu Duc <diemannschaft2003@gmail.com>
2024-12-08 23:39:44 +0700

    Config for push notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,17 +1,47 @@
 import { ErrorProvider } from '@/utils/ctx';
 import { SocketProvider } from '@/utils/socket.ctx';
 import { Slot } from 'expo-router';
+import {NotificationProvider} from "@/context/NotificationContext";
 // import { TextEncoder } from 'text-encoding';
+import * as Notifications from "expo-notifications";
+import * as TaskManager from "expo-task-manager";
 
 // global.TextEncoder = TextEncoder;
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: false,
+        shouldSetBadge: false,
+    }),
+});
+
+const BACKGROUND_NOTIFICATION_TASK = "BACKGROUND-NOTIFICATION-TASK";
+
+TaskManager.defineTask(
+    BACKGROUND_NOTIFICATION_TASK,
+    ({ data, error, executionInfo }) => {
+        console.log("✅ Received a notification in the background!", {
+            data,
+            error,
+            executionInfo,
+        });
+        // Do something with the notification data
+    }
+);
+
+Notifications.registerTaskAsync(BACKGROUND_NOTIFICATION_TASK);
+
 
 export default function Root() {
     // Set up the auth context and render our layout inside of it.
     return (
-        <ErrorProvider>
-            <SocketProvider>
-                <Slot />
-            </SocketProvider>
-        </ErrorProvider>
+        <NotificationProvider>
+            <ErrorProvider>
+                <SocketProvider>
+                    <Slot />
+                </SocketProvider>
+            </ErrorProvider>
+        </NotificationProvider>
     );
 }
