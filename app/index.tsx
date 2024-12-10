f491d477406f504a0c4395e483056c76752c4725
Nguyen Huu Duc <diemannschaft2003@gmail.com>
2024-12-10 14:14:06 +0700

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,131 @@
-import { ROLES } from '@/constants/Roles';
-import { getProfileLocal } from '@/services/storages/profile';
-import { getTokenLocal } from '@/services/storages/token';
-import { TProfile } from '@/types/profile';
-import { Redirect } from 'expo-router';
-import { useEffect, useState } from 'react';
-import {Text, View} from 'react-native';
-import { TextEncoder } from 'text-encoding';
-import {useNotification} from "@/context/NotificationContext";
+import { useState, useEffect, useRef } from 'react';
+import { Text, View, Button, Platform } from 'react-native';
+import * as Device from 'expo-device';
+import * as Notifications from 'expo-notifications';
+import Constants from 'expo-constants';
 
-global.TextEncoder = TextEncoder;
-export default function Index() {
-    const { expoPushToken, notification, error } = useNotification();
-    const [isLoading, setIsLoading] = useState(true);
-    const [profile, setProfile] = useState<TProfile | undefined>(undefined);
-    const [token, setToken] = useState<string | null | undefined>(undefined);
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+        shouldShowAlert: true,
+        shouldPlaySound: true,
+        shouldSetBadge: true,
+    }),
+});
+
+
+
+async function sendPushNotification(expoPushToken: string) {
+    const message = {
+        to: expoPushToken,
+        sound: 'default',
+        title: 'Original Title',
+        body: 'And here is the body!',
+        data: { someData: 'goes here' },
+    };
+
+    await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Accept-encoding': 'gzip, deflate',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(message),
+    });
+}
+
+
+function handleRegistrationError(errorMessage: string) {
+    alert(errorMessage);
+    throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+    if (Platform.OS === 'android') {
+        Notifications.setNotificationChannelAsync('default', {
+            name: 'default',
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: '#FF231F7C',
+        });
+    }
+
+    if (Device.isDevice) {
+        const { status: existingStatus } = await Notifications.getPermissionsAsync();
+        let finalStatus = existingStatus;
+        if (existingStatus !== 'granted') {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+        }
+        if (finalStatus !== 'granted') {
+            handleRegistrationError('Permission not granted to get push token for push notification!');
+            return;
+        }
+        const projectId =
+            Constants?.expoConfig?.extra?.eas?.projectId ?? Constants?.easConfig?.projectId;
+        if (!projectId) {
+            handleRegistrationError('Project ID not found');
+        }
+        try {
+            const pushTokenString = (
+                await Notifications.getExpoPushTokenAsync({
+                    projectId,
+                })
+            ).data;
+            console.log(pushTokenString);
+            return pushTokenString;
+        } catch (e: unknown) {
+            handleRegistrationError(`${e}`);
+        }
+    } else {
+        handleRegistrationError('Must use physical device for push notifications');
+    }
+}
+
+export default function App() {
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState<Notifications.Notification | undefined>(
+        undefined
+    );
+    const notificationListener = useRef<Notifications.Subscription>();
+    const responseListener = useRef<Notifications.Subscription>();
+
     useEffect(() => {
-        const p1 = getProfileLocal().then((data) => setProfile(data));
-        const p2 = getTokenLocal().then((token) => setToken(token));
-        Promise.all([p1, p2]).finally(() => setIsLoading(false));
+        registerForPushNotificationsAsync()
+            .then(token => setExpoPushToken(token ?? ''))
+            .catch((error: any) => setExpoPushToken(`${error}`));
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            notificationListener.current &&
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            responseListener.current &&
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
     }, []);
-    if (isLoading) {
-        return <Text>Loading...</Text>;
-    }
-    if (token && profile) {
-        console.log(profile);
-        if (profile.role == ROLES.LECTURER)
-            return <Redirect href={'/lecturer'} />;
-        else return <Redirect href={'/student'} />;
-    }
+
     return (
-        <View>
-            <Redirect href={'/(auth)/sign-in'} />
-            <Text>{JSON.stringify(notification?.request.content.data, null, 2)}</Text>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'space-around' }}>
+            <Text>Your Expo push token: {expoPushToken}</Text>
+            <View style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Title: {notification && notification.request.content.title} </Text>
+                <Text>Body: {notification && notification.request.content.body}</Text>
+                <Text>Data: {notification && JSON.stringify(notification.request.content.data)}</Text>
+            </View>
+            <Button
+                title="Press to Send Notification"
+                onPress={async () => {
+                    await sendPushNotification(expoPushToken);
+                }}
+            />
         </View>
     );
 }
